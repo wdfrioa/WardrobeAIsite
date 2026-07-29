efe7a81232f04f6aaee61125dcbe0480c27449a8
Дмитрий <db136899@gmail.com>
2026-07-30 00:28:45 +0300

    setup github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: { unoptimized: true },
+// };
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
+  output: 'export',              // статический экспорт
+  basePath: '/WardrobeAIsite',   // название твоего репо
+  assetPrefix: '/WardrobeAIsite/',
+  images: {
+    unoptimized: true,           // GitHub Pages не поддерживает оптимизацию картинок
+  },
+  trailingSlash: true,
 };
+
 module.exports = nextConfig;
